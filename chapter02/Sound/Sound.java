eea8635b2ad776e62fe67191a3ce4649f4716ce9
Christopher Duggan <christopherduggan@gmail.com>
2019-11-26 22:10:36 -0500

    create Sound

---
+++ chapter02/Sound/Sound.java
@@ -0,0 +1,7 @@
+class Sound {
+  public static void main(String args[]) {
+    double interval = 7.2;
+    double feet = interval * 1100;
+    System.out.println("The time between the lightning strike and the sound of thunder was " + interval + ". That means the lightning struck " + feet + " feet away!");
+  }
+}
