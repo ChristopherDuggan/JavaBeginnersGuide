96a6b0dda95eff0621463020e0fb5845b4a8a23a
Christopher Duggan <christopherduggan@gmail.com>
2019-11-28 15:32:35 -0500

    test previously added java, correct sntyax errors in LogicalOpTable

---
+++ chapter02/DistanceFromEcho/Echo.java
@@ -0,0 +1,7 @@
+class DistanceFromEcho {
+  public static void main(String args[]) {
+    int time = 10;
+    double distance = time * 550; //1100/2
+    System.out.println("Your echo bounced off an object " + distance + " feet away!");
+  }
+}
--- chapter02/LogicalOpTable/LogicalOpTable.java
+++ chapter02/LogicalOpTable/LogicalOpTable.java
@@ -1,15 +1,16 @@
 class LogicalOpTable {
   public static void main(String args[]) {
-    boolean p = q = true;
-    System.out.println("| p \t| q \t| p == q \t| p!=q \t| p & q \t| p | q \t| p ^ q \t| p || q \t| p && q \t| !p \t|");
+    boolean p = true;
+    boolean q = true;
+    System.out.println("p \tq\tp == q\tp!=q\tp & q\tp | q\tp ^ q \tp || q\tp && q\t!p\t");
     for (int i = 0; i < 4; i++) {
-      if (i = 1) q = false;
-      if (i = 2) {
+      if (i == 1) q = false;
+      if (i == 2) {
         p = false;
         q = true;
       }
-      if (i = 3) q = false;
-      System.out.println("| " + p + "\t|" + q + "\t|" p == q + "\t|" + p != q + "\t| " + p & q + "\t| " + p | q + "\t|" + p ^ q + "\t|" + p || q + "\t|" + p && + "\t|" + !p + "\t|");
+      if (i == 3) q = false;
+      System.out.println(p + "\t" + q + "\t" + (p == q) + "\t" + (p !=q) + "\t" + (p & q) + "\t" + (p | q) + "\t" + (p ^ q) + "\t" + (p || q) + "\t" + (p && q) + "\t" + !p);
     }
   }
 }
