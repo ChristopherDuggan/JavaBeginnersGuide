7abba34552febb1fa6131637c6a43023563bbbec
Christopher Duggan <christopherduggan@gmail.com>
2019-11-30 17:06:59 -0500

    GuessThe Letter -allow more than one guess, limit guesses to 5 tries

--- chapter03/GuessTheLetter/GuessTheLetter.java
+++ chapter03/GuessTheLetter/GuessTheLetter.java
@@ -1,25 +1,37 @@
 class GuessTheLetter {
   public static void main(String args[])
     throws java.io.IOException {
-    char ch, answer = 'k';
+    char ch, ignore, answer = 'k';
 
     System.out.println("I'm thinking of a letter between a and z.");
-    System.out.print("Can you guess it?: ");
+    System.out.println("Can you guess it in 5 tries?: ");
 
-    ch = (char) System.in.read();
-    if (ch == answer) System.out.println("Right!");
-    else { System.out.print("Sorry, your answer was too ");
-      if (ch < answer) System.out.println("low");
-      else System.out.println("high");
-      System.out.println("The letter was " + answer);
+    for (int i = 0; i < 5; i++) {
+      ch = (char) System.in.read();
+      do {
+        ignore = (char) System.in.read();
+      } while (ignore != '\n');
+
+      if (ch == answer){
+        System.out.println("Right!");
+        break;
+      }
+      else { System.out.print("Sorry, your answer was too ");
+        if (ch < answer) {
+          System.out.println("low");
+        }
+        else {
+          System.out.println("high");
+        }
+      }
+      System.out.println("That was guess number " + (i+1));
     }
+        System.out.println("The letter was " + answer);
   }
 }
 
 /* todo
  * pick the letter at random
- * limit them to 5 tries
- * after 5 wrong answers, reveal the correct answer
  * ask if they'd like to play again (y or n)
  * if y, start over
  * if n, exit
