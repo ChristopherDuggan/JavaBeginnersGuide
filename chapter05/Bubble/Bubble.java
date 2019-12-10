8a2dac242ef64a6adb5801dd14a5217cf2099e05
Christopher Duggan <chris@Christophers-MacBook-Air.local>
2019-12-10 18:39:09 -0500

    replace \size\ variable with nums.length in Bubble

--- chapter05/Bubble/Bubble.java
+++ chapter05/Bubble/Bubble.java
@@ -2,17 +2,14 @@ class Bubble {
   public static void main(String args[]) {
     int[] nums = { 99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49 };
     int a, b, temp;
-    int size;
-
-    size = 10;
 
     System.out.print("Original array is: ");
-    for(int i = 0; i < size; i++)
+    for(int i = 0; i < nums.length; i++)
       System.out.print(" " + nums[i]);
     System.out.println();
 
-    for(a = 1; a < size; a++)
-      for(b = size-1; b >= a; b--) {
+    for(a = 1; a < nums.length; a++)
+      for(b = nums.length-1; b >= a; b--) {
         if(nums[b-1] > nums[b]) {
           temp = nums[b-1];
           nums[b-1] = nums[b];
@@ -20,7 +17,7 @@ class Bubble {
         }
       }
     System.out.print("Sorted array is: ");
-    for(int i = 0; i < size; i++) {
+    for(int i = 0; i < nums.length; i++) {
       System.out.print(" " + nums[i]);
     }
       System.out.println();
